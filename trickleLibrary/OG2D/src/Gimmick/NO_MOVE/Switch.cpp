7d4b99a3466d0b603064f4f66abed40a0d34bfde
Jastin <takumiyokoo@gmail.com>
2018-04-13 01:46:49 +0900

    今まで個人のブランチで上げていたSourceを上げました

--- trickleLibrary/OG2D/src/Gimmick/NO_MOVE/Switch.cpp
+++ trickleLibrary/OG2D/src/Gimmick/NO_MOVE/Switch.cpp
@@ -1,82 +1,106 @@
 #include "Switch.h"
 using namespace std;
+
+//ジャスティン風
+//const int IMAGE_SIZE_X = 64;                    //画像サイズ_X
+//const int IMAGE_SIZE_Y = 64;                    //画像サイズ_Y
 //
-//const int POS_X = 500;
-//const int POS_Y = 270;
+//const int POS_X = IMAGE_SIZE_X * 19;            //初期座標値（現在使用はしていません）
+//const int POS_Y = IMAGE_SIZE_Y * 8;             //初期座標値（現在使用はしてません）
 //
-//const int IMAGE_SIZE = 128;
-////☆☆☆☆//-----------------------------------------------------------------------------
-//Switch::Switch(/*EnemyHitTest* e_pointa*/)
+//												//☆☆☆☆//-----------------------------------------------------------------------------
+//Switch::Switch()                                //コンストラクタ（引数なし）                      
 //{
-//	//player_pointa = p_pointa;
-//	//enemy_pointa = e_pointa;
-//	_filePath = "Switch.bmp";
-//	flag = false;
-//	switchflag = false;
+//
 //}
 ////☆☆☆☆//-----------------------------------------------------------------------------
-//Switch::~Switch()
+//Switch::Switch(Vec2 pos)                        //コンストラクタ（Vec2　初期座標値）           
 //{
-//	//delete player_pointa;
-//	//delete enemy_pointa;
+//	position = pos;                             //初期座標値をSwicthの座標値に代入する
+//	Pos.push_back(pos);                         //引数で受け取った座標値をvectorに保存する
+//}
+////☆☆☆☆//-----------------------------------------------------------------------------
+//Switch::~Switch()                               //デストラクタ　現在は特になし
+//{
+//
 //}
 ////☆☆☆☆//-----------------------------------------------------------------------------
 //void Switch::Initlaize()
 //{
-//	/*_filePath = "Switch.bmp";
-//	flag = false;
-//	switchflag = false;*/
-//
-//	image.TextureCreate(_filePath);
-//	//collisionCube.hitBase = Box2D((int)position.x, (int)position.y, 32, 32);
-//	CreateObject(Objform::Cube, Vec2(POS_X, POS_Y), Vec2(IMAGE_SIZE, IMAGE_SIZE), 0);
-//}
-////☆☆☆☆//-----------------------------------------------------------------------------
-//void Switch::Update()
-//{
-//	//CheakHit(enemy_pointa);
-//}
-////☆☆☆☆//-----------------------------------------------------------------------------
-//void Switch::Finalize()
-//{
-//	image.Finalize();
-//}
-////☆☆☆☆//-----------------------------------------------------------------------------
-//void Switch::Render()
-//{
-//	Box2D draw((int)position.x, (int)position.y, IMAGE_SIZE, IMAGE_SIZE);
-//	draw.OffsetSize();
-//	Box2D src(0,0, IMAGE_SIZE,IMAGE_SIZE);
-//	image.Draw(draw, src);
-//}
-////☆☆☆☆//-----------------------------------------------------------------------------
-//void Switch::CheakHit(Object* objhit)
-//{
-//	//flag = collisionCube.hitBox(objhit->collisionCube);
-//	flag = hit(*objhit);
-//
-//	//スイッチが押せる場所にいるかどうか？
-//	if (flag)
+//	for (int i = 0; i < 2; ++i)                //スイッチの設置分ループさせる
 //	{
-//		//cout << "true" << endl;
-//		// Sキーを押してSwitchをON/OFFの切り替えをする
-//		//if (Input::KeyInputDown(Input::S))
-//		//{
-//			if (switchflag)
-//			{
-//				switchflag = false;
-//				cout << "スイッチoff" << endl;
-//			}
-//			else
-//			{
-//				switchflag = true;
-//				cout << "スイッチon" << endl;
-//			}
-//		//}
+//		switch_ON_OFF[i] = false;
+//		hitBace[i].CreateObject(Objform::Cube, Pos[i], Vec2(IMAGE_SIZE_X, IMAGE_SIZE_Y), 0);         //当たり判定矩形を生成する
 //	}
+//	CheakHit();
+//}
+////☆☆☆☆//-----------------------------------------------------------------------------
+//void Switch::UpDate()                      //更新処理                      
+//{
+//
+//}
+////☆☆☆☆//-----------------------------------------------------------------------------
+//void Switch::Finalize()                    //解放処理
+//{
+//}
+////☆☆☆☆//-----------------------------------------------------------------------------
+//void Switch::Render()                      //描画処理
+//{
+//
+//}
+////追加変数//-----------------------------------------------------------------------------
+//void Switch::CheakHit()                                 //当たり判定の処理をまとめた関数
+//{
+//	hitBace[0].CollisionProcess = [&](const Object& o_)                                                          //当たり判定処理                
+//	{
+//		if (o_.objectTag == "PlayerFoot" || o_.objectTag == "PlayerLeft" || o_.objectTag == "PlayerRight")
+//		{
+//			if (gameEngine->in.down(Input::KeyBoard::Q))
+//			{
+//				if (switch_ON_OFF[0])
+//				{
+//					cout << "OFF" << endl;
+//					switch_ON_OFF[0] = false;
+//				}
+//				else
+//				{
+//					cout << "ON" << endl;
+//					switch_ON_OFF[0] = true;
+//				}
+//				((Senpuki&)o_).Switch_Swap(this, 0);
+//			}
+//		}
+//	};
+//
+//	hitBace[1].CollisionProcess = [&](const Object& o_)                                                          //当たり判定処理                
+//	{
+//		if (o_.objectTag == "PlayerFoot" || o_.objectTag == "PlayerLeft" || o_.objectTag == "PlayerRight")
+//		{
+//			if (gameEngine->in.down(Input::KeyBoard::Q))
+//			{
+//				if (switch_ON_OFF[1])
+//				{
+//					cout << "OFF" << endl;
+//					switch_ON_OFF[1] = false;
+//				}
+//				else
+//				{
+//					cout << "ON" << endl;
+//					switch_ON_OFF[1] = true;
+//				}
+//				//((Senpuki&)o_).Switch_Swap(this, 1); アクセス違反の原因になっています。
+//			}
+//		}
+//	};
+//}
+//void Switch::Set_Pos(const Vec2 pos)
+//{
+//	Pos.push_back(pos);                                 //引数の座標値を追加する
 //}
 
 
+
+//横田さん風
 void Switch::Initialize(Vec2 pos) {
 	position = pos;
 	CreateObject(Cube, position, Vec2(64, 64), 0.0f);
--- trickleLibrary/OG2D/src/Gimmick/NO_MOVE/Switch.h
+++ trickleLibrary/OG2D/src/Gimmick/NO_MOVE/Switch.h
@@ -1,39 +1,108 @@
 //______________________________//
 //|動くギミック  スイッチ      |//
-//|履歴：2018/03/27 横尾	   |//
-//|　                          |//
+//|履歴：2018/03/29 横尾	   |//
+//|履歴：2018/03/31 横尾　     |//
+//|履歴：2018/04/01 横尾       |//
+//|履歴: 2018/04/02 横尾       |//
+//|履歴: 2018/04/03 横尾       |//
+//|履歴：2018/04/07 横尾       |//
 //|____________________________|//
 #pragma once
 #include "Object\Object.h"
+#include "Player\Player.h"
 #include "Senpuki.h"
-//#include "Player\Player.h"
-//#include "Player\EnemyHitTest.h"
+#include <vector>
 
-//class EnemyHitTest;
 //class Player;
-
+//class Senpuki;
+//
 //class Switch : public Object
 //{
 //public:
-//	Switch(/*EnemyHitTest* e_pointa*/);
-//	~Switch();
+//	/*
+//	コンストラクタ　※現在は何も処理がありません
+//	■ 引数   : なし
+//	■ 戻り値 : なし
+//	*/
+//	explicit Switch();
+//
+//
+//	/*
+//	コンストラクタ（Vec2　初期座標値）
+//	■ 引数   : Vec2 スイッチの初期座標値設定
+//	■ 戻り値 : なし
+//	*/
+//	explicit Switch(Vec2);
+//
+//
+//	/*
+//	デストラクタ　なし
+//	■ 引数   : なし
+//	■ 戻り値 : なし
+//	*/
+//	virtual ~Switch();
+//
+//
+//	/*
+//	(引数1 初期座標)初期化処理　当たり判定・矩形の生成・フラグの初期化
+//	■ 引数   : なし
+//	■ 戻り値 : なし
+//	*/
 //	void Initlaize();
-//	void Update();
+//
+//
+//	/*
+//	更新処理
+//	■ 引数   : なし
+//	■ 戻り値 : なし
+//	*/
+//	void UpDate();
+//
+//
+//	/*
+//	解放処理
+//	■ 引数   : なし
+//	■ 戻り値 : なし
+//	*/
 //	void Finalize();
+//
+//
+//	/*
+//	描画処理（今のところ意味がない）
+//	■ 引数   : なし
+//	■ 戻り値 : なし
+//	*/
 //	void Render();
 //
-//	void CheakHit(Object* objhit);
+//
+//	/*
+//	当たり判定の処理を書いたところ
+//	■ 引数   : なし
+//	■ 戻り値 : なし
+//	*/
+//
+//	void CheakHit();
+//
+//
+//	/*
+//	スイッチの当たり判定座標値をvectorで保存する関数
+//	■ 引数   : const Vec2 当たり判定に必要な座標値
+//	■ 戻り値 : なし
+//	*/
+//	void Set_Pos(const Vec2);
+//
+//public:
+//
+//	bool switch_ON_OFF[2];                       //スイッチがONかOFFかを示す ON(true) OFF(false)
+//	Object hitBace[2];                           //スイッチの当たり判定矩形
 //private:
-//	Texture image;
-//	std::string _filePath;
-//	//Player* player_pointa;
-//	//EnemyHitTest* enemy_pointa;
 //
-//	//今後はビット演算で行う
-//
-//	bool flag;//当たり判定
-//	bool switchflag;//スイッチがONかOFFかを示す
+//	bool Hitflag;                                //当たり判定
+//	std::vector<Vec2>Pos;                        //コンストラクタ時に引数できた座標値を保存する
 //};
+
+
+//横田さん風
 
 //注意！！！！
 //プレイヤが二つ以上のスイッチに同時に接触するとエラー吐きます
