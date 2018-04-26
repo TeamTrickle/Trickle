dbd7cd855884e3afe90a28aedc0ec16c932fae05
Jastin <takumiyokoo@gmail.com>
2018-04-26 22:41:42 +0900

    タイマークラスの更新をしました

--- trickleLibrary/OG2D/src/GameProcessManagement/Timer.cpp
+++ trickleLibrary/OG2D/src/GameProcessManagement/Timer.cpp
@@ -12,22 +12,6 @@ void Timer::Initialize()
 {
 	//ŠÔ‚Ìƒ[ƒƒNƒŠƒA
 	frame_time = 0;               //GetTime‚ÌŠÔ‚ğ‘ã“ü‚·‚é•Ï”
-	frame_time_sec = 0;           //•b
-	frame_time_min = 0;           //•ª
-	frame_time_hours = 0;         //
-}
-
-void Timer::Update()
-{
-	
-}
-void Timer::Instrumentation()
-{
-	
-}
-void Timer::Instrumentation_output(Timer& timer_)
-{
-	cout << timer_.frame_time_hours << "ŠÔ" << timer_.frame_time_min << "•ª" << timer_.frame_time_sec << "•b" << endl;
 }
 void Timer::Frame_Set()
 {
