f085245f8d988e218e5ef5198ae844fe2d7ab266
Jastin <takumiyokoo@gmail.com>
2018-05-08 20:24:18 +0900

    製氷機と加熱機が正常に動くようにしました（Game.cppを編集してあります）

--- trickleLibrary/OG2D/src/Gimmick/NO_MOVE/Kanetuki.h
+++ trickleLibrary/OG2D/src/Gimmick/NO_MOVE/Kanetuki.h
@@ -9,20 +9,18 @@ class Kanetuki : public Object
 public:
 	Kanetuki();
 	~Kanetuki();
-	bool Create(Vec2, Vec2);	//座標値　当たり判定サイズ
-	void CheckHit();			//当たり判定のみする
-	void Set_pointa(Water*);	//マネージャーからWaterだけを全て抜きとる
-	void Set_pointa();	//マネージャーからWaterだけを全て抜きとる
+	void Create(Vec2, Vec2);	//座標値　当たり判定サイズ
+	void SetWaterPool(std::vector<Water*> *);
+	void Update();
 	Object hitBace;
 private:
-	bool Initital;						//初期化処理を1度しかしないために・・・
 	const int Fire_time_LIQUID = 70;	//加熱する時間 液体
 	const int Fire_time_SOLID = 40;	    //加熱する時間 個体
 	int Fire_movetime;					//燃やす時間を格納する
-	std::vector <Water*>w_vec;
-	bool Createflag;
-};
+	
+	//typedef std::vecor<Water*> WaterPool;
+	std::vector<Water*> *water;		
 
-//cm にアドレスを渡す
-//Create()をする
-//Initialize()をする
+	//状態遷移
+	void toSteam(Water*);		//水から水蒸気にする
+};
