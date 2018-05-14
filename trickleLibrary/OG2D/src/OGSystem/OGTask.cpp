5169c4df241a8e9df27cc3219cf6a4012bc3d6fa
kanekona <amausakonata1932@gmail.com>
2018-05-14 23:32:14 +0900

    起動確認

--- trickleLibrary/OG2D/src/OGSystem/OGTask.cpp
+++ trickleLibrary/OG2D/src/OGSystem/OGTask.cpp
@@ -3,7 +3,8 @@
 void OGTK::_myGameInitialize()
 {
 	//¶¬‚·‚éWindowî•ñ
-	OGge->SetWindow(960, 540, "WindowName", false);	
+	//OGge->SetWindow(960, 540, "WindowName", false);	
+	OGge->SetWindow(1920, 1080, "Trickel");
 }
 
 void OGTK::StartTaskObject()
