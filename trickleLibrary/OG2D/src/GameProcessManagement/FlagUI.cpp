a10ea51324cb14930e3e71ebbacb4c246d90082a
Jastin <takumiyokoo@gmail.com>
2018-05-17 22:06:31 +0900

    ミッションフラグUIを追加しました

---
+++ trickleLibrary/OG2D/src/GameProcessManagement/FlagUI.cpp
@@ -0,0 +1,86 @@
+#include "FlagUI.h"
+
+
+bool FlagUI::Initialize(Vec2& pos)
+{
+	std::cout << "ミッション達成エフェクト　初期化" << std::endl;
+	this->taskName = "Ster";
+	this->Init(taskName);
+	CreateObject(Cube, pos, Vec2(64, 64), 0);
+	//フラグの初期化
+	Resultflag = 0;
+	flagactive = false;
+	SetDrawOrder(0.1f);
+	image[0].Create((std::string)"Ster.jpg");
+	image[1].Create((std::string)"SterB.jpg");
+	return true;
+}
+void FlagUI::UpDate()
+{
+	//フラグを代入する
+	this->SetResultflag();
+}
+void FlagUI::Render2D()
+{
+	Box2D draw(position, Scale);
+	draw.OffsetSize();
+	Box2D src = this->Src;
+	src.OffsetSize();
+	if (flagactive)
+	{
+		image[0].Draw(draw, src);
+	}
+	else
+	{
+		image[1].Draw(draw, src);
+	}
+}
+bool FlagUI::Finalize()
+{
+	for (int i = 0; i < 2; ++i)
+	{
+		image[i].Finalize();
+	}
+	return true;
+}
+void FlagUI::SetResultflag()
+{
+	auto result = OGge->GetTask<Result>("Result");
+	Resultflag = result->Get_Flag();
+}
+void FlagUI::FalgJudge(int judge)
+{
+	int flag = 0;
+	flag |= Resultflag;
+	if ((flag & judge) == judge)
+	{
+		flagactive = true;
+	}
+}
+FlagUI::FlagUI()
+{
+	std::cout << "ミッション達成エフェクト　生成" << std::endl;
+}
+FlagUI::~FlagUI()
+{
+	this->Finalize();
+	std::cout << "ミッション達成エフェクト　解放" << std::endl;
+}
+FlagUI::SP FlagUI::Create(Vec2& pos,bool flag_)
+{
+	FlagUI::SP to = FlagUI::SP(new FlagUI());
+	if (to)
+	{
+		to->me = to;
+		if (flag_)
+		{
+			OGge->SetTaskObject(to);
+		}
+		if (!to->Initialize(pos))
+		{
+			to->Kill();
+		}
+		return to;
+	}
+	return nullptr;
+}
