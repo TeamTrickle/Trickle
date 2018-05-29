91db71d47edc85c0c2a9825fbb024dde71f0b7ab
kanekona <amausakonata1932@gmail.com>
2018-05-30 07:29:46 +0900

    水の落下速度が速かったので修正

--- trickleLibrary/OG2D/src/Water/water.h
+++ trickleLibrary/OG2D/src/Water/water.h
@@ -88,7 +88,7 @@ public:
 private:
 	const float MOVE_SPEED = 15.f;								//移動スピード
 	const float JUMP_POWER = -20.f;								//ジャンプパワー
-	const float MAX_FALL = 30.f;								//落下最大速度
+	const float MAX_FALL = 15.f;								//落下最大速度
 	const float GRAVITY = (9.8f / 60.f / 60.f * 32) * 5;		//重力加速度
 	const float FIN_SPEED = 5.0f;								//摩擦
 	const int RAIN_TIME = 180;
