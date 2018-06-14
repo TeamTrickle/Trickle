4ee73fb61b373ad72e16f0484cb05288e9ef74e5
Heewon Kim <nabicore@icloud.com>
2018-06-14 10:40:05 +0900

    読み込み方式を変更

--- trickleLibrary/OG2D/src/Gimmick/NO_MOVE/TimeSign.cpp
+++ trickleLibrary/OG2D/src/Gimmick/NO_MOVE/TimeSign.cpp
@@ -1,11 +1,12 @@
 #include "TimeSign.h"
+#include "GameProcessManagement\GameProcessManagement.h"
 
 
 TimeSign::~TimeSign() {
 	Finalize();
 }
 
-TimeSign::SP TimeSign::Create(const Vec2& p, bool flag_ = true) {
+TimeSign::SP TimeSign::Create(const std::string& bp, const Vec2& p, bool flag_ = true) {
 	TimeSign::SP to = TimeSign::SP(new TimeSign());
 	if (to)
 	{
@@ -14,7 +15,7 @@ TimeSign::SP TimeSign::Create(const Vec2& p, bool flag_ = true) {
 		{
 			OGge->SetTaskObject(to);
 		}
-		if (!to->Initialize(p))
+		if (!to->Initialize(bp, p))
 		{
 			to->Kill();
 		}
@@ -23,9 +24,12 @@ TimeSign::SP TimeSign::Create(const Vec2& p, bool flag_ = true) {
 	return nullptr;
 }
 
-bool TimeSign::Initialize(const Vec2& pos) {
+bool TimeSign::Initialize(const std::string& basePath, const Vec2& pos) {
+	base.Create(basePath);
+	Vec2 baseSize = base.GetTextureSize();
+	originPos.w = baseSize.x;
+	originPos.h = baseSize.y;
 	setPosition(pos);
-	setAtlasPath(atlasPath, extension);
 
 	__super::Init((std::string)"timesign");
 	__super::SetDrawOrder(0.5f);
@@ -33,49 +37,64 @@ bool TimeSign::Initialize(const Vec2& pos) {
 }
 
 void TimeSign::UpDate() {
-
+	auto timeSrc = OGge->GetTask<GameManager>((std::string)"GM");
+	if (timeSrc) {
+		currentTime[0] = timeSrc->MinuteTime() / 10;
+		currentTime[1] = timeSrc->MinuteTime() % 10;
+		currentTime[2] = timeSrc->SecondsTime() / 10;
+		currentTime[3] = timeSrc->SecondsTime() % 10;
+	}
 }
 
 void TimeSign::Render2D() {
+	// ”wŒi‰æ‘œ
+	{
+		Box2D draw = originPos;
+		draw.OffsetSize();
+		Box2D src(0.f, 0.f, originPos.w, originPos.h);
+		src.OffsetSize();
+		base.Draw(draw, src);
+	}
 
+	// ”š
+	for (int i = 0; i < timerDraws.size(); ++i) {
+		Box2D draw = timerDraws[i];
+		draw.OffsetSize();
+		Box2D src = numberSrcs[currentTime[i]];
+		src.OffsetSize();
+		numberAtlas.Draw(draw, src);
+	}
 }
 
 void TimeSign::Finalize() {
 	ClearNumberAtlas();
+	base.Finalize();
 }
 
 void TimeSign::ClearNumberAtlas() {
-	for (auto& a : numberAtlas) {
-		a.second.Finalize();
-	}
-	spacer.Finalize();
-	numberAtlas.clear();
-
-	originPos.w = 0;
-	originPos.h = 0;
+	numberAtlas.Finalize();
+	numberSrcs.clear();
 }
 
-void TimeSign::setAtlasPath(const std::string& path, const std::string& ext) {
-	atlasPath = path;
-	extension = ext;
+void TimeSign::setAtlas(const std::string& path, const Box2D& lettersize) {
 	ClearNumberAtlas();
-
-	for (int i = 0; i < 10; ++i) {
-		Texture number;
-		number.Create(path + std::to_string(i) + extension);
-		numberAtlas.insert({ i, number });
-	}
-	spacer.Create(path + "spacer" + extension);
+	numberAtlas.Create(path);
 	
-	Vec2 tileSize = numberAtlas[0].GetTextureSize();
-	originPos.w = tileSize.x;
-	originPos.h = tileSize.y;
+	int number = 0;
+	Box2D numberBox = Box2D(0.f , lettersize.y, lettersize.w, lettersize.h);
+	while (number < 10) {
+		numberSrcs.insert({
+			number,
+			numberBox
+		});
+		number += 1;
+		numberBox.x += lettersize.x;
+	}
 }
 
 void TimeSign::setAtlasAngle(const float& a) {
-	for (auto& n : numberAtlas)
-		n.second.Rotate(a);
-	spacer.Rotate(a);
+	rotateAngle = a;
+	numberAtlas.Rotate(a);
 }
 
 void TimeSign::setPosition(const Vec2& p) {
@@ -85,4 +104,10 @@ void TimeSign::setPosition(const Vec2& p) {
 
 Vec2& TimeSign::getPosition() const {
 	return Vec2(originPos.x, originPos.y);
+}
+
+void TimeSign::setDialogPos(const Vec2& pos) {
+	timerDraws[0].x = pos.x;
+	timerDraws[0].y = pos.y;
+	setAtlasAngle(rotateAngle);
 }
--- trickleLibrary/OG2D/src/Gimmick/NO_MOVE/TimeSign.h
+++ trickleLibrary/OG2D/src/Gimmick/NO_MOVE/TimeSign.h
@@ -11,7 +11,7 @@
 
 class TimeSign : public TaskObject {
 private:
-	bool Initialize(const Vec2&);
+	bool Initialize(const std::string&, const Vec2&);
 	virtual void UpDate() override;
 	virtual void Render2D() override;
 	void Finalize();
@@ -26,25 +26,30 @@ public:
 	/**
 	 @brief		数字画像ファイルたちが入ってるフォルダパスを再指定します
 	 @param		画像パス
-	 @param		ファイル拡張子 (「.」までつけて）
-	 @note		フォルダの中で数字画像ファイルたちの名前は必ずその数字と同じく設定してください　（例えば１を表す画像なら1.png)
-	 @note		すべての画像は同じサイズになるように制作してください
-	 @note		タイムを区切る「：」も数字たちと同じサイズで「space.png」的な名前で入れて下さい
+	 @param		1文字当たりのサイズ
 	 */
-	void setAtlasPath(const std::string&, const std::string&);
+	void setAtlas(const std::string&, const Box2D&);
+
+	/**
+	 @brief		時計の中、数字の位置を指定します
+	 @param		位置値
+	 */
+	void setDialogPos(const Vec2&);
 
 	void setPosition(const Vec2&);
 	Vec2& getPosition() const;
 
 	typedef std::shared_ptr<TimeSign> SP;
-	static SP Create(const Vec2&, bool);
+	static SP Create(const std::string&, const Vec2&, bool);
 
 private:
 	std::array<Box2D, 4>			timeNumberDraw;
 	Box2D							originPos;
 	
-	Texture							spacer;
-	std::map<int, Texture>			numberAtlas;
-	std::string						atlasPath = "numbers/";
-	std::string						extension = ".png";
+	Texture							base;
+	Texture							numberAtlas;
+	std::map<int, Box2D>			numberSrcs;
+	std::array<Box2D, 4>			timerDraws;
+	std::array<int, 4>				currentTime;
+	float							rotateAngle = 0.f;
 };
