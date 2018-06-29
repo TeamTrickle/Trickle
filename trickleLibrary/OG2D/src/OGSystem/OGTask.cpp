fa5171183f7b08374c3f091f782e0a27d2bc53e8
kanekona <amausakonata1932@gmail.com>
2018-06-29 20:45:22 +0900

    OGTask本番用とデータ初期化

--- trickleLibrary/OG2D/src/OGSystem/OGTask.cpp
+++ trickleLibrary/OG2D/src/OGSystem/OGTask.cpp
@@ -4,13 +4,14 @@
 #include "Task\Task_TitleMovement.h"
 #include "Task\StageSelect.h"
 #include "Task\Task_Result.h"
+#include "GameProcessManagement\GameProcessManagement.h"
 
 void OGTK::_myGameInitialize()
 {
 	//生成するWindow情報
-	OGge->SetWindow(960, 540, "WindowName", false);	
-	//OGge->SetWindow(1920, 1080, "Trickel", true);
-	//OGge->SetWindowPos(Vec2(0, 0));
+	//OGge->SetWindow(960, 540, "WindowName", false);	
+	OGge->SetWindow(1920, 1080, "Trickel", true);
+	OGge->SetWindowPos(Vec2(0, 0));
 	//マウスカーソル不可視
 	OGge->SetCursorOn(false);
 }
@@ -25,6 +26,9 @@ void OGTK::StartTaskObject()
 
 	//タスクの初期化
 	auto TopTask = Title::Create();
+	//*MapNum = 6;
+	//Game::Create();
+	//GameManager::ResetData();
 }
 
 OGTK::~OGTK()
