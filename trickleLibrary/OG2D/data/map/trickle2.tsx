03144b0c9ef792b491d797fad8c64c476ba5adb0
yokota0717 <17ci0139@gmail.com>
2018-07-25 07:24:59 +0900

    チュートリアルマップ

--- trickleLibrary/OG2D/data/map/trickle2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="trickle2" tilewidth="256" tileheight="256" tilecount="40" columns="20">
- <image source="../image/mapchip2.png" width="5120" height="512"/>
-</tileset>
