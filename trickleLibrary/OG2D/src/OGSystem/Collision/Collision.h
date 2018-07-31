fe2400249b72d72cd436526701bb32ac3fe08a1d
kanekona <amausakonata1932@gmail.com>
2018-08-01 06:10:32 +0900

    SystemUpDate

--- trickleLibrary/OG2D/src/OGSystem/Collision/Collision.h
+++ trickleLibrary/OG2D/src/OGSystem/Collision/Collision.h
@@ -1,3 +1,4 @@
+///Box2D,Circleを使用し当たり判定を行うclass
 #pragma once
 #include "OGSystem\_OGsystem.h"
 
@@ -7,9 +8,9 @@ class CollisionBox
 {
 public:
 	Box2D hitBase;						//当たり判定	
-	bool hitBox(CollisionBox& b);		//矩形との判定
-	bool hitCircle(CollisionCircle& b);	//円との判定
-	void Rotate(float _angle);			//回転を適用
+	bool hitBox(const CollisionBox& b);		//矩形との判定
+	bool hitCircle(const CollisionCircle& b);	//円との判定
+	void Rotate(const float _angle);	//回転を適用
 	float angle;						//回転値
 private:
 	void hitdraw();						//確認用
@@ -18,7 +19,7 @@ class CollisionCircle
 {
 public:
 	Circle hitBase;						//当たり判定
-	bool hitBox(CollisionBox& b);		//矩形との判定
-	bool hitCircle(CollisionCircle& b);	//円との判定
+	bool hitBox(const CollisionBox& b);		//矩形との判定
+	bool hitCircle(const CollisionCircle& b);	//円との判定
 private:
 };
