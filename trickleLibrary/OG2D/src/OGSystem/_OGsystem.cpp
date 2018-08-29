64b775d96217b38601d30ad2323bda4c2333d029
kanekona <amausakonata1932@gmail.com>
2018-08-30 03:56:10 +0900

    System Up Date ・データの暗号化複合化システムの追加 ・デバッグログ出力システムの追加

--- trickleLibrary/OG2D/src/OGSystem/_OGsystem.cpp
+++ trickleLibrary/OG2D/src/OGSystem/_OGsystem.cpp
@@ -152,3 +152,69 @@ void OG::cout(const Color& c)
 {
 	std::cout << "red " << c.red << ":green " << c.green << ":blue " << c.blue << ":alpha " << c.alpha << "\n";
 }
+bool OG::Data_Cipher(const std::string& in_path, const std::string& out_path)
+{
+	char c;
+	std::ifstream ifs(in_path, std::ios::in | std::ios::binary);
+	if (!ifs) {
+		return false;
+	}
+
+	std::ofstream ofs(out_path, std::ios::in | std::ios::binary);
+
+	int i = 0;
+
+	while (ifs.get(c)) {
+		if (c != '\n')
+		{
+			c = c ^ randomCipher[i];//データcの内容を暗号化
+		}
+		ofs << c;
+		++i;
+		if (i >= 40) {
+			i = 0;
+		}
+	}
+	ifs.close();
+	ofs.close();
+	return true;
+}
+std::string OG::Data_Composite(std::ifstream& ifs)
+{
+	std::istreambuf_iterator<char> it(ifs);
+	std::istreambuf_iterator<char> last;
+	std::string str(it, last);
+	int j = 0;
+	for (int i = 0; i < str.size(); ++i)
+	{
+		if (str[i] != '\n')
+		{
+			str[i] = str[i] ^ randomCipher[j];
+		}
+		++j;
+		if (j >= 40) {
+			j = 0;
+		}
+	}
+	return str;
+}
+void OG::OutDebugData(const std::string& out_path, const std::string& text)
+{
+	std::string path = "./data/debug/" + out_path;
+	std::ofstream ofs(path, std::ios::app | std::ios::binary);
+	if (!ofs)
+	{
+		std::cout << "ファイルが存在しない/生成に失敗しました\n";
+	}
+	ofs << text;
+	ofs.close();
+}
+void OG::DataClear(const std::string& path)
+{
+	std::ofstream ofs(path, std::ios::trunc);
+	if (!ofs)
+	{
+		std::cout << "ファイルは存在しません\n";
+	}
+	ofs.close();
+}
